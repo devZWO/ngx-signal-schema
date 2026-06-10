b9d544ed38d120b843cd87b9433633f80acdff94
ConstantinMueller <constantin.mueller@devzwo.com>
2026-06-10 23:07:52 +0200

    add documentation workspace for ngx-signal-schema using Astro Starlight

---
+++ docs/astro.config.mjs
@@ -0,0 +1,63 @@
+// @ts-check
+import {defineConfig} from 'astro/config';
+import starlight from '@astrojs/starlight';
+import starlightTypeDoc from 'starlight-typedoc'
+
+
+// https://astro.build/config
+export default defineConfig({
+    site: 'https://devzwo.github.io',
+    base: '/ngx-signal-schema',
+    integrations: [
+        starlight({
+            title: 'ngx-signal-schema',
+            social: [{icon: 'github', label: 'GitHub', href: 'https://github.com/devZWO/ngx-signal-schema'}],
+            plugins: [
+                // Generate the documentation.
+                starlightTypeDoc({
+                    entryPoints: ['../projects/ngx-signal-schema/src/public-api.ts'],
+                    tsconfig: '../projects/ngx-signal-schema/tsconfig.lib.json',
+                    typeDoc: {
+                        router: 'category',
+                    }
+                }),
+            ],
+            sidebar: [
+                {
+                    label: 'Welcome', slug: 'index',
+                },
+                {
+                    label: 'Getting Started', link: 'gettingstarted'
+                },
+                {
+                    label: 'Concepts',
+                    items: [
+                        // Each item here is one entry in the navigation menu.
+                        {label: 'Schema Composition', slug: 'concepts/schemacomposition'},
+                        {label: 'Conditional Schemas', slug: 'concepts/schemacomposition'},
+                        {label: 'Reusable Validators', slug: 'guides/example'},
+                    ],
+                },
+                {
+                    label: 'Recipes',
+                    items: [
+                        // Each item here is one entry in the navigation menu.
+                        {label: 'Contact Form', slug: 'guides/example'},
+                        {label: 'Dynamic Company vs Person Form', slug: 'guides/example'},
+                    ],
+                },
+                {
+                    label: 'API',
+                    items: [
+                        {label: 'Composition', items: [{autogenerate: {directory: 'api/Composition'}}]},
+                        {label: 'Validators', items: [{autogenerate: {directory: 'api/Validators'}}]},
+                        {label: 'Conditions', items: [{autogenerate: {directory: 'api/Conditions'}}]},
+                        {label: 'Rules', items: [{autogenerate: {directory: 'api/Rules'}}]},
+                        {label: 'Other', items: [{autogenerate: {directory: 'api/Other'}}]},
+                        {label: 'deprecated', items: [{autogenerate: {directory: 'api/deprecated'}}]},
+                    ],
+                }
+            ],
+        }),
+    ],
+});
